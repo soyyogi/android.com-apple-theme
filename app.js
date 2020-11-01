bd0a81f2527d70c6f3fe7a691c972173716c243b
yogi <raoyogesh78@yahoo.in>
2020-11-02 00:13:31 +0100

    built express server and organised files using handlebars

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+const port = 3000 || process.env.PORT;
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: '.hbs',
+    defaultLayout: 'index'
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('main', {title: 'Android | The platform pushing what’s possible'});
+});
+
+app.get('/phone', (req, res) => {
+    res.render('phone', {
+        layout: 'phone',
+        title: 'Phones & Tablets | Android'});
+});
+
+app.listen(port);
